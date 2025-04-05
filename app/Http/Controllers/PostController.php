a4557b77b57144a4d720ebdd3525264fb9c73b6d
Harsha <harshachandim92@gmail.com>
2025-04-05 12:21:36 +0530

    Create controller for post and defined in it route

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function showPostCreateForm(){
+        return 'I am html basic form';
+    }
+}
